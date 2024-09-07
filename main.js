af8e90c11a8ab8abfb73b95c4aa626350ee67685
Pinkify_ <69760484+IgorHuelle@users.noreply.github.com>
2024-09-07 15:03:43 +0200

    Patch v0.5

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-const ofertItems = document.querySelectorAll('.oferts-item')
+const ofertItems = document.querySelectorAll('.oferts-item');
+const ofertInfos = document.querySelectorAll('.oferts-item > .info');
 
 for(let i = 0; i < ofertItems.length; i++) {
-    ofertItems[i].addEventListener('mouseenter', function() {
-        this.classList.add('open-info')
-    })
-    ofertItems[i].addEventListener('mouseleave', function() {
-        this.classList.remove('open-info')
-    })
-}
+    ofertItems[i].addEventListener('mouseenter', () => {
+        ofertItems[i].classList.add('flip-icon');
+        ofertInfos[i].classList.add('open-info');
+    });
+    ofertItems[i].addEventListener('mouseleave', () => {
+        ofertItems[i].classList.remove('flip-icon');
+        ofertInfos[i].classList.remove('open-info');
+    });
+};
